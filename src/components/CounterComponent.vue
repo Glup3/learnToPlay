11df359278369122f5cefbac260ba0032329a438
Phuc Tran <glup3.tran@gmail.com>
2022-09-10 17:02:28 +0200

    load data from localstorage

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -1,18 +1,71 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, onMounted, watch } from 'vue'
 
 const GOD_NUMBER = 0.5
 const GOD_MULTIPLIER = 2
+const GOD_KEY = 'GOD_COUNTERS'
+
+type CounterData = {
+  learnCount: number
+  playCount: number
+  date: string
+}
+
+function isCounterData(o: any): o is CounterData {
+  return 'date' in o && 'learnCount' in o && 'playCount' in o
+}
 
 interface State {
   learnCount: number
   playCount: number
+  isLoading: boolean
 }
 
 const state: State = reactive({
   learnCount: 0,
   playCount: 0,
+  isLoading: true,
 })
+
+onMounted(() => {
+  const dataString = localStorage.getItem(GOD_KEY)
+  if (!dataString) {
+    state.isLoading = false
+    return
+  }
+
+  try {
+    const data = JSON.parse(dataString)
+    if (isCounterData(data)) {
+      if (isToday(new Date(data.date))) {
+        state.learnCount = data.learnCount
+        state.playCount = data.playCount
+      }
+    }
+  } catch (e) {
+    localStorage.removeItem(GOD_KEY)
+  } finally {
+    state.isLoading = false
+  }
+})
+
+watch(state, (newState) => {
+  const dataString = JSON.stringify({
+    learnCount: newState.learnCount,
+    playCount: newState.playCount,
+    date: new Date().toJSON(),
+  } as CounterData)
+  localStorage.setItem(GOD_KEY, dataString)
+})
+
+function isToday(date: Date) {
+  const today = new Date()
+  return (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  )
+}
 
 function incrementLearnCount() {
   state.learnCount = state.learnCount + GOD_NUMBER
@@ -42,12 +95,20 @@ function decrementPlayCount() {
     <div>learnCount: {{ state.learnCount }}h</div>
     <div>playCount: {{ state.playCount }}h</div>
     <div>
-      <button @click="incrementLearnCount">increment learn</button>
-      <button @click="decrementLearnCount">decrement learn</button>
+      <button :disabled="state.isLoading" @click="incrementLearnCount">
+        increment learn
+      </button>
+      <button :disabled="state.isLoading" @click="decrementLearnCount">
+        decrement learn
+      </button>
     </div>
     <div>
-      <button @click="incrementPlayCount">increment play</button>
-      <button @click="decrementPlayCount">decrement play</button>
+      <button :disabled="state.isLoading" @click="incrementPlayCount">
+        increment play
+      </button>
+      <button :disabled="state.isLoading" @click="decrementPlayCount">
+        decrement play
+      </button>
     </div>
   </div>
 </template>
